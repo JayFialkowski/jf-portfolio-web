9d74adbc8dcf406d76bc317923d2207df29ce2f7
Jay Fialkowski <jayfialkowski@me.com>
2021-12-28 12:07:54 -0500

    add stubs

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import {StubComponent} from './stub.component';
 
 const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/home'
+    },
+    {
+        path: 'home',
+        pathMatch: 'full',
+        component: StubComponent
+    },
     {
         path: 'resume',
         pathMatch: 'full',
         loadChildren: () => import('./modules/resume/resume.module').then(m => m.ResumeModule)
+    },
+    {
+        path: 'contact',
+        pathMatch: 'full',
+        component: StubComponent
     }
 ];
 
--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,9 +5,10 @@ import {AppComponent} from './app.component';
 import {HeaderModule} from '@web-portfolio/ui';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
+import {StubComponent} from './stub.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, StubComponent],
     imports: [AppRoutingModule, BrowserModule, HeaderModule],
     providers: [],
     bootstrap: [AppComponent]
