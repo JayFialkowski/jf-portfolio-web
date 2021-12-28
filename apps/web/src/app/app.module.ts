75ae4163da02088e1315cedf982b18a981dc5b79
Jay Fialkowski <jayfialkowski@me.com>
2021-12-27 19:41:33 -0500

    add static external links to header

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderModule} from '@web-portfolio/ui';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, HeaderModule, RouterModule.forRoot([])],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- libs/ui/src/lib/header/header.module.ts
+++ libs/ui/src/lib/header/header.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header.component';
-
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [HeaderComponent],
+    imports: [RouterModule],
     exports: [HeaderComponent]
 })
 
